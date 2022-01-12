ed743b8bee6879b6494f97fda8553569d769b845
Nabil Bout <62855614+nabilbout21@users.noreply.github.com>
2022-01-12 21:32:34 +0000

    Update index.tsx

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -80,13 +80,12 @@ const BookCard: React.FC<BookCardProps> = (Props) => {
       <a
         target="blank"
         rel="noopener"
-        href = CPAlink
+        href="https://www.google.com/"
+        
         
         
         style={
-          saleInfo.saleability === "FOR_SALE"
-            ? undefined
-            : { background: "#E67A00" }
+          { background: "#E67A00" }
         }
       >
         Download Now
